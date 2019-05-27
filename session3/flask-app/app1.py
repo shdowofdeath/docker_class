66853b88dc647ff363a16c875a5939f3e5df777f
eefrat <elhay.efrat@intel.com>
2019-05-27 17:07:06 +0300

    fixing requierment
    Signed-off-by: eefrat <elhay.efrat@intel.com>

--- session3/flask-app/app1.py
+++ session3/flask-app/app1.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 
 # list of cat images
 images = [
-    "https://firebasestorage.googleapis.com/v0/b/docker-curriculum.appspot.com/o/catnip%2F0.gif?alt=media&token=0fff4b31-b3d8-44fb-be39-723f040e57fb",
     "https://firebasestorage.googleapis.com/v0/b/docker-curriculum.appspot.com/o/catnip%2F1.gif?alt=media&token=2328c855-572f-4a10-af8c-23a6e1db574c",
     "https://firebasestorage.googleapis.com/v0/b/docker-curriculum.appspot.com/o/catnip%2F10.gif?alt=media&token=647fd422-c8d1-4879-af3e-fea695da79b2",
     "https://firebasestorage.googleapis.com/v0/b/docker-curriculum.appspot.com/o/catnip%2F11.gif?alt=media&token=900cce1f-55c0-4e02-80c6-ee587d1e9b6e",
